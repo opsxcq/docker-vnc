f0d1b06849a56589822170b7b3ce9360139379dd
OPSXCQ <opsxcq@strm.sh>
2016-12-03 18:06:41 +0000

    vnc fix

--- main.sh
+++ main.sh
@@ -9,3 +9,9 @@ then
     mkdir /root/.vnc
     x11vnc -storepasswd test /root/.vnc/passwd
 fi
+
+Xvfb -screen 0 800x600x16 -ac &
+sleep 15
+env DISPLAY=:0.0 x11vnc -forever -display :0 &
+env DISPLAY=:0.0 fluxbox 
+
