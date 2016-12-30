fb9649e6e24911904933f52b648c57cb31f93919
OPSXCQ <opsxcq@strm.sh>
2016-12-29 22:40:10 -0200

    child runner added

--- main.sh
+++ main.sh
@@ -3,6 +3,12 @@
 # Start vfb
 # Start vnc
 
+if [ -z "$VNC_PASSWORD" ]
+then
+    echo '[-] Please inform a password via VNC_PASSWORD variable'
+    exit -1
+fi
+
 # This cannot be done during install, except if we want a static password
 if [ ! -f /root/.vnc/passwd ]
 then
@@ -14,4 +20,9 @@ Xvfb -screen 0 800x600x16 -ac &
 sleep 15
 env DISPLAY=:0.0 x11vnc -forever -display :0 &
 env DISPLAY=:0.0 fluxbox 
+
+if [ -f /app.sh ]
+then
+    /app.sh
+fi
 
